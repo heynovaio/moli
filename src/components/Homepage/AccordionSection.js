15ae85d8cf1e5779dcc09ca65be9ff5b4dc4562b
Kathryn Lancashire <kathryn@klancashire.com>
2023-05-29 13:33:37 -0700

    Rebuilt accordian that makes more sense

--- src/components/Homepage/AccordionSection.js
+++ src/components/Homepage/AccordionSection.js
@@ -1,14 +1,21 @@
-import * as React from "react"
+import React, { useState } from "react";
 import { useStaticQuery, graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 // import { Accordion, DetailsImage, DetailsContent } from '../../styles/pages/home.module.scss'
 import * as sty from '../../styles/components/accordian.module.scss'
 import TextBox from "../textbox"
 
-const AccordionSection = (props) => {
+
+const AccordionSection = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
   const data = useStaticQuery(graphql`
     query {
-      main_image: file(relativePath: {eq: "what-we-do.jpg"}) {
+      detail_3: file(relativePath: {eq: "what-we-do.jpg"}) {
         childImageSharp {
           gatsbyImageData(
             width: 749
@@ -38,54 +45,71 @@ const AccordionSection = (props) => {
       }
     }
   `)
-  const image = getImage(data.main_image)
+
+  const items = [
+    {
+      title: "Detailing",
+      content: "Moli Industries uses the world leading Tekla Structures software to create extremely accurate and highly detailed 3D models of all our projects. These models are subsequently used to output shop & fabrication drawings, and the information is also transferred digitally to our beam manufacturing lines for automated measuring, cutting and drilling operations.",
+      alt: "A 3d model",
+      image: getImage(data.detail_1)
+    },
+    {
+      title: "Fabrication",
+      content: "Our facility is CWB certified to W47.1 Division 1, with work performed by certified Journeyman Welders. Our shop features multiple state of the art, fully automated beam lines that incorporate high speed carbide drilling, large format band sawing and powerful plasma coping for accuracy to one tenth of a millimeter. ",
+      alt: "A welder working on a steel beam",
+      image:  getImage(data.detail_2)
+    },
+    {
+      title: "Installation",
+      content: "Our highly skilled, CWB certified Journeymen Field crew has successfully erected a wide array of structural projects in Calgary and surrounding area, Western Canada and beyond. The accuracy of the manufactured products from our Shop allows our crew the capability to efficiently tackle any size project.",
+      alt: "A welder working",
+      image:  getImage(data.detail_3)
+    },
+    {
+      title: "Miscellaneous Steel",
+      content: "At Moli Industries we can accommodate and produce a wide selection of miscellaneous steel requests including modifications and upgrades to serve our clients. Common services include ladders, handrails, stairs and many other mild steel products.",
+      alt: "A steel staircase",
+      image:  getImage(data.detail_4)
+    }
+  ];
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
+    return (
+      <>
+      <details>
+        <summary onClick={() => onTitleClick(index)}>{item.title}</summary>
+        {item.content}
+      </details>
+      </>
+      // <div key={item.title}>
+      //   <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+      //     <i className="dropdown icon"></i>
+      //     {item.title}
+      //   </div>
+      //   <div className={`content ${active}`}>
+      //     <p>{item.content}</p>
+      //   </div>
+      // </div>
+    );
+  });
+
   return (
     <section>
       <div className="Container">
-        <div className="FlexWrap" style={{alignItems: 'flex-start', justifyContent: 'space-between', gap: '0'}}>
-          <div style={{width: '50%', height: '100%'}}></div>
-          <div className={sty.ContentBox}>
-            <TextBox>
-              <h2>Our Services</h2>
-              <div className={sty.Accordion}>
-                <details className={sty.Details1} open>
-                  <div className={`${sty.DetailsImage} ${sty.DetailsImage_1}`}>
-                    <GatsbyImage image={getImage(data.detail_1)} alt="A 3d model"/>
-                  </div>
-                  <summary>Detailing</summary>
-                    Moli Industries uses the world leading Tekla Structures software to create extremely accurate and highly detailed 3D models of all our projects. These models are subsequently used to output shop & fabrication drawings, and the information is also transferred digitally to our beam manufacturing lines for automated measuring, cutting and drilling operations.
-                </details>
-                <details className={sty.Details2}>
-                  <summary>Fabrication</summary>
-                  <div className={`${sty.DetailsImage} ${sty.DetailsImage_2}`}>
-                    <GatsbyImage image={getImage(data.detail_2)} alt="A welder working on a steel beam"/>
-                  </div>
-                  Our facility is CWB certified to W47.1 Division 1, with work performed by certified Journeyman Welders. Our shop features multiple state of the art, fully automated beam lines that incorporate high speed carbide
-  drilling, large format band sawing and powerful plasma coping for accuracy to one tenth of a millimeter. 
-                </details>
-                <details className={sty.Details3}>
-                  <summary>Installation</summary>
-                    <div className={`${sty.DetailsImage} ${sty.DetailsImage_3}`}>
-                      <GatsbyImage image={image} alt="A welder working"/>
-                    </div>
-                  Our highly skilled, CWB certified Journeymen Field crew has successfully erected a wide array of structural projects in Calgary and surrounding area, Western Canada and beyond. The accuracy of the manufactured products from our Shop allows our crew the capability to efficiently tackle any size project.
-                </details>
-                <details className={sty.Details4}>
-                  <summary>Miscellaneous Steel</summary>
-                  <div className={`${sty.DetailsImage} ${sty.DetailsImage_4}`}>
-                  <GatsbyImage image={getImage(data.detail_4)} alt="A steel staircase"/>
-                  </div>
-                  At Moli Industries we can accommodate and produce a wide selection of miscellaneous steel requests including modifications and upgrades to serve our clients. Common services include ladders, handrails, stairs and many other mild steel products.
-                </details>
-              </div>
-              
-            </TextBox>
+        <h2>Our Services</h2>
+        <div className={sty.Accordion}>
+          <div className={sty.AccordionImage}>
+            <GatsbyImage image={items[activeIndex].image} alt={items[activeIndex].alt}/>
+          </div>
+          <div className={sty.AccordionInfo}>
+            {renderedItems}
           </div>
         </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default AccordionSection
+export default AccordionSection;
